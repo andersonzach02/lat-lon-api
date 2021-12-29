681e7cd361d39976dac29c97e74bd3a76710dd57
Zachary Anderson <andersonzach02@gmail.com>
2021-12-28 19:10:42 -0500

    Added a couple more tests to the HTMLParser to verfiy it's functionality. Also, left a failing test for parsing data from a table to a JSON object so that I know where to continue from when I pick this up next.

--- utils/HTMLParser.js
+++ utils/HTMLParser.js
@@ -1,13 +1,17 @@
 const cheerio = require('cheerio');
 
 class HTMLParser {
-  constructor(htmlString) {
-    this.$ = cheerio.load(htmlString, null, false);
-  }
+	constructor(htmlString) {
+		this.$ = cheerio.load(htmlString, null, false);
+	}
 
-  getElement(elementType) {
-    return this.$(elementType).html() || '';
-  }
+	getElement(elementType) {
+		return this.$(elementType).html() || '';
+	}
+
+	parseTabletoJSON(tableBody) {
+		return null;
+	}
 }
 
 module.exports = HTMLParser;
