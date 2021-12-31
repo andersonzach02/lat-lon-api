255bdd1221aaf6133d57ad347d21f123f4ae3927
Zachary Anderson <andersonzach02@gmail.com>
2021-12-31 13:34:15 -0500

    Working on the JSON parsing for the HTMLParser. Need to finish testing and implementing the JSON parsing function.

--- test/HTMLParser.test.js
+++ test/HTMLParser.test.js
@@ -2,73 +2,85 @@ const HTMLParser = require('../utils/HTMLParser');
 
 /* eslint-disable no-undef */
 describe('HTMLParser getElement tests', () => {
-	test('getElement should return the element specified by the element type passed as a string', () => {
-		const parser = new HTMLParser('<h2>Test</h2>');
+  test('getElement should return the element specified by the element type passed as a string', () => {
+    const parser = new HTMLParser('<h2>Test</h2>');
 
-		const header = parser.getElement('h2');
+    const header = parser.getElement('h2');
 
-		expect(header).toBe('Test');
-	});
+    expect(header).toBe('Test');
+  });
 
-	test('getElement should only return the specified element when multiple HTML tags are present', () => {
-		const parser = new HTMLParser('<h1>Test</h1><h2>This is a test</h2>');
+  test('getElement should only return the specified element when multiple HTML tags are present', () => {
+    const parser = new HTMLParser('<h1>Test</h1><h2>This is a test</h2>');
 
-		const header = parser.getElement('h1');
+    const header = parser.getElement('h1');
 
-		expect(header).toBe('Test');
-	});
+    expect(header).toBe('Test');
+  });
 
-	test('getElement should return items when a table is requested', () => {
-		const parser = new HTMLParser(
-			'<table><tbody><tr><td>Row 1</td></tr><tr><td>Row 2</td></tr></tbody></table>'
-		);
+  test('getElement should return items when a table is requested', () => {
+    const parser = new HTMLParser(
+      '<table><tbody><tr><td>Row 1</td></tr><tr><td>Row 2</td></tr></tbody></table>'
+    );
 
-		const tbody = parser.getElement('table');
+    const tbody = parser.getElement('table');
 
-		expect(tbody).toBe(
-			'<tbody><tr><td>Row 1</td></tr><tr><td>Row 2</td></tr></tbody>'
-		);
-	});
+    expect(tbody).toBe(
+      '<tbody><tr><td>Row 1</td></tr><tr><td>Row 2</td></tr></tbody>'
+    );
+  });
 
-	test('getElement with empty string should return an empty string', () => {
-		const parser = new HTMLParser('<h1>Test</h1>');
+  test('getElement with empty string should return an empty string', () => {
+    const parser = new HTMLParser('<h1>Test</h1>');
 
-		const response = parser.getElement('');
+    const response = parser.getElement('');
 
-		expect(response).toBe('');
-	});
+    expect(response).toBe('');
+  });
 
-	test('getElement on a parser with no html string should return empty string', () => {
-		const parser = new HTMLParser('');
+  test('getElement on a parser with no html string should return empty string', () => {
+    const parser = new HTMLParser('');
 
-		const response = parser.getElement('h1');
+    const response = parser.getElement('h1');
 
-		expect(response).toBe('');
-	});
+    expect(response).toBe('');
+  });
 
-	test('getElement should return first row when given a table and given a <tr> tag', () => {
-		const parser = new HTMLParser(
-			'<table><tbody><tr><td>Row 1</td></tr><tr><td>Row 2</td></tr></tbody></table>'
-		);
+  test('getElement should return first row when given a table and given a <tr> tag', () => {
+    const parser = new HTMLParser(
+      '<table><tbody><tr><td>Row 1</td></tr><tr><td>Row 2</td></tr></tbody></table>'
+    );
 
-		const row = parser.getElement('tr');
+    const row = parser.getElement('tr');
 
-		expect(row).toBe('<td>Row 1</td>');
-	});
+    expect(row).toBe('<td>Row 1</td>');
+  });
 });
 
 describe('HTMLParser parseTabletoJSON tests', () => {
-	test('parseTabletoJSON should take a table with a single cell and convert the data in that row to a JSON object', () => {
-		const parser = new HTMLParser(
-			'<table><tbody><tr><td>Row 1</td></tr></tbody></table>'
-		);
+  test('parseTabletoJSON should take a table with a single cell and convert the data in that row to a JSON object where the key is the column number', () => {
+    const parser = new HTMLParser(
+      '<table><tbody><tr><td>Row 1</td></tr></tbody></table>'
+    );
 
-		const tableBody = parser.getElement('table');
+    const tableBody = parser.getElement('table');
 
-		const tableData = parser.parseTabletoJSON(tableBody);
+    const tableData = parser.parseTabletoJSON(tableBody);
 
-		expect(tableData).toBe({ 1: 'Row 1' });
-	});
+    expect(tableData).toStrictEqual({ 1: 'Row 1' });
+  });
 
-	//also want table with multiple rows to return an array of objects, maybe the single row should be an array too with a single object
+  test('parseTabletoJSON should take a table with multiple columns in a single row and covert the data to a JSON where the key is the column number', () => {
+    const parser = new HTMLParser(
+      '<table><tbody><tr><td>Row 1</td><td>Row 2</td></tr></tbody></table>'
+    );
+
+    const tableBody = parser.getElement('table');
+
+    const tableData = parser.parseTabletoJSON(tableBody);
+
+    expect(tableData).toStrictEqual({ 1: 'Row 1', 2: 'Row 2' });
+  });
+
+  // also want table with multiple rows to return an array of objects, maybe the single row should be an array too with a single object
 });
--- utils/HTMLParser.js
+++ utils/HTMLParser.js
@@ -1,17 +1,35 @@
 const cheerio = require('cheerio');
 
 class HTMLParser {
-	constructor(htmlString) {
-		this.$ = cheerio.load(htmlString, null, false);
-	}
+  constructor(htmlString) {
+    this.$ = cheerio.load(htmlString, null, false);
+  }
 
-	getElement(elementType) {
-		return this.$(elementType).html() || '';
-	}
+  getElement(elementType) {
+    return this.$(elementType).html() || '';
+  }
 
-	parseTabletoJSON(tableBody) {
-		return null;
-	}
+  // eslint-disable-next-line class-methods-use-this
+  parseTabletoJSON(tableBody) {
+    const table = cheerio.load(tableBody, null, false);
+
+    const rowInfo = [];
+
+    table('td').each((i) => {
+      rowInfo[i] = table(this).text();
+    });
+
+    console.log(rowInfo);
+
+    return rowInfo.map(
+      (previous, colInfo, index) => ({ ...previous, [index]: colInfo }),
+      {}
+    );
+
+    // return {
+    //   1: rowInfo,
+    // };
+  }
 }
 
 module.exports = HTMLParser;
